2e3092d8800ad0b68f3792f8cb581a0041c050f2
Devansh Bhardwaj <dbhardwaj123411@gmail.com>
2026-03-24 11:31:16 +0530

    /api/auth/login added with bcrypt with json web tokens

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 exports.registerUser = async (req, res) => {
   try {
@@ -33,5 +35,34 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({
       message: error.message,
     });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and Password are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+    res.status(200).json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "server error" });
   }
 };
